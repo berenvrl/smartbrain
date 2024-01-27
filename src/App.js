fc1477ba3873bd9f84d6b693d2f51ab341199047
Beren Varol <beren.vrl@gmail.com>
2024-01-27 20:02:54 +0300

    fetch is modified

--- src/App.js
+++ src/App.js
@@ -66,7 +66,8 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({ imageUrl: this.state.input });
-    fetch("http://localhost:3000/imageurl", {
+
+    fetch("https://mybackend-d39z.onrender.com/imageurl", {
       method: "post",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
@@ -76,7 +77,7 @@ class App extends Component {
       .then((response) => response.json())
       .then((response) => {
         if (response) {
-          fetch("http://localhost:3000/image", {
+          fetch("https://mybackend-d39z.onrender.com/image", {
             method: "put",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
